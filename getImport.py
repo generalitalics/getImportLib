e22204738dd8a3217eab5cc0c02fb77c360d9f22
generalitalics <generalitalics@mail.ru>
2016-08-29 12:42:47 +0300

    Update getImport.py

--- getImport.py
+++ getImport.py
@@ -1,89 +1,94 @@
-# Byte-compiled / optimized / DLL files
-__pycache__/
-*.py[cod]
-*$py.class
+__autor__ = 'm.shoshin'
 
-# C extensions
-*.so
+# -*- coding: utf-8 -*-
 
-# Distribution / packaging
-.Python
-env/
-build/
-develop-eggs/
-dist/
-downloads/
-eggs/
-.eggs/
-lib/
-lib64/
-parts/
-sdist/
-var/
-*.egg-info/
-.installed.cfg
-*.egg
+import os
+import re
+import sys
+import shutil
 
-# PyInstaller
-#  Usually these files are written by a python script from a template
-#  before PyInstaller builds the exe, so as to inject date/other infos into it.
-*.manifest
-*.spec
+from datetime import datetime
 
-# Installer logs
-pip-log.txt
-pip-delete-this-directory.txt
+print('\n$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n\n')
 
-# Unit test / coverage reports
-htmlcov/
-.tox/
-.coverage
-.coverage.*
-.cache
-nosetests.xml
-coverage.xml
-*,cover
-.hypothesis/
+time_start = datetime.now()
 
-# Translations
-*.mo
-*.pot
 
-# Django stuff:
-*.log
-local_settings.py
+source = os.path.join(".", sys.argv[1])
+#source = sys.argv[1] - путь к файлу
 
-# Flask stuff:
-instance/
-.webassets-cache
 
-# Scrapy stuff:
-.scrapy
+directory = sys.argv[1]
+files = os.listdir(directory)
+print(directory)
+path_f = []
+for d, dirs, files in os.walk(directory):
+    for f in files:
+        path = os.path.join(d, f) # формирование адреса
+        path_f.append(path) # добавление адреса в список
+print(path_f)
+pyFiles = filter(lambda x: x.endswith('.py'), path_f)
+for i in pyFiles:
+    print("|   ", '{:<120}'.format(i), "|")
+    # with open(i) as f:
+    #     try:
+    #         lines = f.readlines()
+    #         print("lines:", lines)
+    #     except:
+    #         print("Ошибка")
+    #     # result = re.finditer(r"", lines)
+    # # for match in result:
+    # #     print
+    # #     match.groups()
 
-# Sphinx documentation
-docs/_build/
+# print(files)
+data = None
+# with open(source) as f:
+#     lines = f.readlines()
 
-# PyBuilder
-target/
 
-# IPython Notebook
-.ipynb_checkpoints
+res_dir = os.path.join(".", "for_Lera")
+print('Running      : [' + sys.argv[0] + ']')
+print('Conversion   : [' + sys.argv[1] + ']')
+print('In directory : [' + res_dir + ']\n')
+print('Wait...',)
 
-# pyenv
-.python-version
-
-# celery beat schedule file
-celerybeat-schedule
-
-# dotenv
-.env
-
-# virtualenv
-venv/
-ENV/
-
-# Spyder project settings
-.spyderproject
-
-# Rope project settings
-.ropeproject
+if not os.path.exists(res_dir):
+    os.makedirs(res_dir)
+# makedirs - создает папку по директории с результатами, если ее нету
+# result - ключ + значение
+result = {}
+#line - это строка во всем lines
+# for line in lines:
+# в переменную l записывается list со значениями из каждого line
+#     l = line.split(";")
+#     key = l[4]
+#     value = l[1]
+#print key
+# с помощью регулярных выражений убираем лишнее
+#   key = re.sub(r"\d+-\d", "", key)
+#   key = re.sub(r"\d+$", "", key)
+    #начало магии
+    #вроде что-то типо проверки на отличную от других типов строк
+    #if result.get(key, False) is False:
+    #   result[key] = []
+    #print result.get(key, False)
+    # result - это список со значениями через знак "|"
+    #result[key].append("|".join(l))
+    # with open(os.path.join(".", res_dir, key + ".csv"), "a") as w:
+    #     w.write(value + '\n')
+# соединяет
+'''
+for k, s in result.iteritems():
+    #print s
+    text = "".join(k)
+    #print text
+    # конец магии
+    with open(os.path.join(".", res_dir, k + ".csv"), "w") as w:
+        w.write(text)
+'''
+print('OK')
+# сколько выполнялась это чудесная програмуля
+time_end = datetime.now()
+time_delta = time_start - time_end
+print("Total time: %d s" % -time_delta.total_seconds())
